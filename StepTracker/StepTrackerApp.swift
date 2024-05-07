e452a579c151109ca8846513f9235a02c8a0dfb4
Alexey Kremnev <alkremn@gmail.com>
2024-05-06 23:30:50 -0600

    Created xcode project

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Alexey Kremnev on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
